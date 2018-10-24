ef31f2a687c2a0c9501dc28bbb9ea8a7afc1027e
airo8899 <diykovaa@gmail.com>
2018-10-24 22:43:40 +0300

    First commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some number");
+            func(Console.ReadLine());
+            Console.ReadKey();
+
+            void func(string line)
+            {
+                var res = line == "42" ? "Hello, World!" : "42";
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
